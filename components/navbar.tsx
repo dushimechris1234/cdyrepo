6065ee1b1a911aeaaa3666241ba136cff57146ef
Berwamihigo <learncanvaa@gmail.com>
2025-05-17 10:07:13 +0200

    asdasdasd

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,7 +57,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className={`fixed top-0 left-0 right-0 z-50 bg-gray-300 transition-all duration-300 text-white`}>
+      <nav className={`fixed top-0 left-0 right-0 z-50 bg-gray-200 transition-all duration-300 text-white`}>
         <div className="container mx-auto px-4 py-4">
           <div className="flex items-center justify-between">
             {/* Logo */}
